9a78a8be313de72c1aa0e811d28ec6db5dc82a87
Markus Scholz <scholz.mr@gmail.com>
2025-12-31 14:36:27 +0100

    adds command line arguments handling

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import sys
 from stats import get_book_text, count_words, count_chars, print_report
 
 def main():
-    url = "./books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    url = sys.argv[1]
     text = get_book_text(url)
     words = count_words(text)
     char_count = count_chars(text)
